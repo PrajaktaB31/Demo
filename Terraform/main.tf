4037cc1d5fcbdda17b9b5897d1cb716b416db9a9
PrajaktaB31 <PrajaktaBhame2@gmail.com>
2024-12-17 17:52:32 +0530

    added variables for secrets

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -9,6 +9,7 @@ terraform {
 
 provider "azurerm" {
   
+
   client_id       = var.client_id         # The Application (client) ID
   client_secret   = var.client_secret      # The client secret you created
   tenant_id       = var.tenant_id          # The Directory (tenant) ID
