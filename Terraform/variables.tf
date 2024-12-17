db2ecc55eb3ef1eaf24ec03a038afff4e166ab0e
PrajaktaB31 <PrajaktaBhame2@gmail.com>
2024-12-17 17:20:40 +0530

    Add .gitignore to exclude Terraform files and directories

---
+++ Terraform/variables.tf
@@ -0,0 +1,20 @@
+variable "client_secret" {
+  description = "Client secret for Azure"
+  type        = string
+}
+
+variable "client_id" {
+  description = "Client ID for Azure"
+  type        = string
+}
+
+variable "tenant_id" {
+    description = "Tennat ID for Azure"
+    type = string
+}
+
+variable "subscription_id" {
+    description = "subscription ID for Azure"
+    type = string
+  
+}
