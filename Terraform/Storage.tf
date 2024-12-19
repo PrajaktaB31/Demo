32449b90a8c79c6e3d853448faee32ebd724e40c
PrajaktaB31 <PrajaktaBhame2@gmail.com>
2024-12-19 21:04:11 +0530

    adding doc

--- Terraform/Storage.tf
+++ Terraform/Storage.tf
@@ -3,6 +3,26 @@ resource "azurerm_storage_account" "san123" {
     resource_group_name = azurerm_resource_group.rg.name
     location = azurerm_resource_group.rg.location
     account_tier = "Standard"
+    public_network_access_enabled = true
     account_replication_type = "LRS"
+  
+}
+
+resource "azurerm_storage_container" "data" {
+    name = "data"
+    storage_account_name = "sanprajakta"
+    container_access_type = "private"
+
+  
+}
+
+resource "azurerm_storage_blob" "blob" {
+    name = "Hello.docs"
+    storage_account_name = "sanprajakta"
+    storage_container_name = "data"
+    type = "Block"
+    source = "Hello.docs"
+
+
   
 }
