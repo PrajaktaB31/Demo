bdc81eb3889e122b66de28117b28a14d175d33ff
PrajaktaB31 <PrajaktaBhame2@gmail.com>
2024-12-20 15:45:31 +0530

    added vm

---
+++ Terraform/nsg.tf
@@ -0,0 +1,26 @@
+resource "azurerm_network_security_group" "name" {
+    name = "nsg-p"
+    location = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule  {
+    name = "Allow_HTTPS"
+    priority = 2000
+    direction = "Inbound"
+    access = "Allow"
+    protocol = "Tcp"
+    source_port_range = "*"
+    destination_port_range = "3389"
+    source_address_prefix = "*"
+    destination_address_prefix = "*"
+
+
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsg-secasso-p" {
+    subnet_id = azurerm_subnet.subnetp1.id
+    network_security_group_id = azurerm_network_security_group.name.id
+
+  
+}
--- Terraform/vm.tf
+++ Terraform/vm.tf
@@ -8,6 +8,7 @@ resource "azurerm_network_interface" "ni-praju" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.subnetp1.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.pubip-praju.id
   }
 }
 
@@ -18,6 +19,7 @@ resource "azurerm_windows_virtual_machine" "vm-praju" {
   size                = "Standard_F2"
   admin_username      = "adminuser"
   admin_password      = "P@$$w0rd1234!"
+  availability_set_id = azurerm_availability_set.availab-p.id
   network_interface_ids = [
     azurerm_network_interface.ni-praju.id,
   ]
@@ -33,4 +35,41 @@ resource "azurerm_windows_virtual_machine" "vm-praju" {
     sku       = "2016-Datacenter"
     version   = "latest"
   }
+
+}
+
+resource "azurerm_public_ip" "pubip-praju" {
+  name = "pub-ip1"
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method = "Static"
+  
+}
+
+resource "azurerm_managed_disk" "name" {
+  name = "manageddisk-p"
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  storage_account_type = "Standard_LRS"
+  create_option  = "Empty"
+  disk_size_gb = 16
+
+}
+
+resource "azurerm_virtual_machine_data_disk_attachment" "vmd-praju" {
+
+managed_disk_id = azurerm_managed_disk.name.id
+virtual_machine_id = azurerm_windows_virtual_machine.vm-praju.id
+lun = "0"
+caching = "ReadWrite"
+
+  
+}
+resource "azurerm_availability_set" "availab-p" {
+  name = "Avail-p"
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  platform_fault_domain_count = 3
+  platform_update_domain_count = 3
+
 }
