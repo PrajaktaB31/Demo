dcf38a589e2d0154e85b33610f1c0ad434707db4
PrajaktaB31 <PrajaktaBhame2@gmail.com>
2024-12-20 11:46:31 +0530

    added vm

--- Terraform/vm.tf
+++ Terraform/vm.tf
@@ -1,37 +1,36 @@
-resource "virtual_network_interface" "nic-p" {
-  name                = "nic-p1"
+resource "azurerm_network_interface" "ni-praju" {
+  name                = "ni-praju"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+
 
   ip_configuration {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.subnetp1.id
     private_ip_address_allocation = "Dynamic"
-
   }
 }
 
-resource "azurerm_virtual_machine" "vm-praju" {
-  name                = "vm-praju1"
+resource "azurerm_windows_virtual_machine" "vm-praju" {
+  name                = "vm-praju"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  size                = "standard_F2"
-  admin_username      = "Praju2331"
-  admin_password      = "Praju@2331"
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_password      = "P@$$w0rd1234!"
   network_interface_ids = [
-    azurerm_network_interface.nic-p.id,
+    azurerm_network_interface.ni-praju.id,
   ]
 
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
-
   }
 
   source_image_reference {
     publisher = "MicrosoftWindowsServer"
     offer     = "WindowsServer"
-    sku       = "2016=Datacenter"
+    sku       = "2016-Datacenter"
     version   = "latest"
   }
 }
